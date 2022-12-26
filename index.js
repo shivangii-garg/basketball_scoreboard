412f005bc982b4c528eb2e6e1ec8f3fc518e3d58
shgarg <shgarg@axway.com>
2022-12-26 22:57:52 +0530

    add score

---
+++ index.js
@@ -0,0 +1,25 @@
+let addOne = () => {
+   let score = document.getElementById("homeScore");
+    score.innerHTML = Number(score.innerHTML) + 1
+}
+let addTwo = () => {
+   let score = document.getElementById("homeScore");
+    score.innerHTML = Number(score.innerHTML) + 2
+}
+let addThree = () => {
+   let score = document.getElementById("homeScore");
+    score.innerHTML = Number(score.innerHTML) + 3
+}
+
+let addOneGuest = () => {
+   let score = document.getElementById("guestScore");
+    score.innerHTML = Number(score.innerHTML) + 1
+}
+let addTwoGuest = () => {
+   let score = document.getElementById("guestScore");
+    score.innerHTML = Number(score.innerHTML) + 2
+}
+let addThreeGuest = () => {
+   let score = document.getElementById("guestScore");
+    score.innerHTML = Number(score.innerHTML) + 3
+}
